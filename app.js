f8e3bd3574e9095998fdc8328ab815cb78724395
ssmauri <seryas_mauri@hotmail.com>
2024-05-30 03:22:10 -0400

    Recode to use UI

--- app.js
+++ app.js
@@ -6,52 +6,50 @@ function getComputerChoice() {
     let computerChoice = Math.floor((Math.random() * 3) + 1 );
     return computerChoice
 }
-function callErrorInput(humanChoice) {
-    alert("Ingresaste " + humanChoice + ". Debes ingresar del 1 al 3");
-    let getNewValue = getHumanChoice();
-    return getNewValue
-}
 
-function getHumanChoice() {
-    let humanChoice = prompt("Inserte 1 = Piedra, 2 = Papel, 3 = Tijera");
-    if ( humanChoice === null ) {
-        throw new Error("NO ERROR. Press F5 to play again");        
-    } else { 
-        if ( humanChoice >= 1 && humanChoice <= 3 ) {
-            return humanChoice
-        } else {
-            return callErrorInput(humanChoice);
-        }
-    }
+function changeLabel (selector, text) {
+    document.querySelector(selector).textContent = text;
+    document.querySelector('div#humanScore').textContent = humanScore;
+    document.querySelector('div#computerScore').textContent = computerScore;
 }
 
 function playRound(computerChoice, humanChoice) {
     const youWin = ['21','32','13']
     if ( computerChoice == humanChoice) {
-        alert("Empate técnico!!\nambos escogieron " + codigos[computerChoice]);
+        changeLabel('div#showMsg', 'Empate técnico!!\nambos escogieron ' + codigos[computerChoice])
     } else {
         let resultFinal =  humanChoice + computerChoice;
-
         if (youWin.includes(resultFinal)) {
-            alert("You WIN!!!\n" + codigos[humanChoice] + " gana a " + codigos[computerChoice]);
             ++humanScore;
+            changeLabel('div#showMsg', "You WIN!!!\n" + codigos[humanChoice] + " gana a " + codigos[computerChoice])
         } else {
-            alert("Pc WINS!!\n" + codigos[computerChoice] + " gana a " + codigos[humanChoice]);
             ++computerScore;
+            changeLabel('div#showMsg', "Pc WINS!!\n" + codigos[computerChoice] + " gana a " + codigos[humanChoice] );
         } 
     }
 }
 
-function playGame(round = 5) {
-    for ( i = 1; i <= round; ++i) {
-        playRound(getComputerChoice(), getHumanChoice());
-    }
-    if ( humanScore > computerScore ) {
-        alert("Human WINS\n" + humanScore + " a " + computerScore);
-    } else {
-        alert("Pc WINS!!\n" + computerScore + " a " + humanScore);
-    }
 
-}
+// JAVASCRIPT for UI RPS-game
 
-playGame();
+const btns = document.querySelectorAll(".game-btn");
+const buttons = document.querySelector(".Buttons");
+
+btns.forEach((button) => {    // El querySelectorAll nos devuelve un NodeList como array por eso usamaos forEach.
+    button.addEventListener("click", () => {
+        playRound(getComputerChoice(), button.id);
+        if (humanScore >= 3) {
+            changeLabel('div#showMsg', 'Human Wins!!!');
+            humanScore = 0;
+            computerScore = 0;
+            buttons.style.display = "none";
+        } else if (computerScore >=3) {
+            changeLabel('div#showMsg', 'Pc Wins!!!');
+            humanScore = 0;
+            computerScore = 0;
+            buttons.style.display = "none";
+        }
+    });
+});
+
+
